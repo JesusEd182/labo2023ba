adf8aaff8a7495de5f3d7532a282bb6e934c41e9
JesusED182 <vielma.jesused182@gmail.com>
2023-09-02 13:43:34 +0000

    321_arboles_azarosos_0.25

--- src/ArbolesAzarosos/321_arboles_azarosos_010.r
+++ src/ArbolesAzarosos/321_arboles_azarosos_010.r
@@ -11,7 +11,7 @@ require("rpart")
 
 # parmatros experimento
 PARAM <- list()
-PARAM$experimento <- 3210
+PARAM$experimento <- 3210.25
 
 # Establezco la semilla aleatoria, cambiar por SU primer semilla
 PARAM$semilla <- 143821
