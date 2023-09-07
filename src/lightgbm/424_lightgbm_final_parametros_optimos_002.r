ad88bac05418a8dd1084eabe7ca25fb03e5b9266
JesusED182 <vielma.jesused182@gmail.com>
2023-09-07 00:52:17 +0000

    424_lightgbm_final_002_12294

--- src/lightgbm/424_lightgbm_final_parametros_optimos_002.r
+++ src/lightgbm/424_lightgbm_final_parametros_optimos_002.r
@@ -134,7 +134,7 @@ setorder(tb_entrega, -prob)
 # suba TODOS los archivos a Kaggle
 # espera a la siguiente clase sincronica en donde el tema sera explicado
 
-cortes <- seq(12294)
+cortes <- (12294)
 for (envios in cortes) {
   tb_entrega[, Predicted := 0L]
   tb_entrega[1:envios, Predicted := 1L]
