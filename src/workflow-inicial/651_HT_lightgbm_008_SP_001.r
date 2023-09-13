07e6040a888746a499a360fc4f751771f5cc1c66
JesusED182 <vielma.jesused182@gmail.com>
2023-09-13 12:12:01 +0000

    651_SP_008_001

--- src/workflow-inicial/651_HT_lightgbm_008_SP_001.r
+++ src/workflow-inicial/651_HT_lightgbm_008_SP_001.r
@@ -38,46 +38,46 @@ PARAM$lgb_semilla <- c(200003, 280001, 300007 ,143821, 168977) # cambiar por su 
 
 
 # Hiperparametros FIJOS de  lightgbm
-PARAM$lgb_basicos <- list(
-  boosting = "gbdt", # puede ir  dart  , ni pruebe random_forest
-  objective = "binary",
-  metric = "custom",
-  first_metric_only = TRUE,
-  boost_from_average = TRUE,
-  feature_pre_filter = FALSE,
-  force_row_wise = TRUE, # para reducir warnings
-  verbosity = -100,
-  max_depth = -1L, # -1 significa no limitar,  por ahora lo dejo fijo
-  min_gain_to_split = 0.0, # min_gain_to_split >= 0.0
-  min_sum_hessian_in_leaf = 0.001, #  min_sum_hessian_in_leaf >= 0.0
-  lambda_l1 = 0.0, # lambda_l1 >= 0.0
-  lambda_l2 = 0.0, # lambda_l2 >= 0.0
-  max_bin = 31L, # lo debo dejar fijo, no participa de la BO
-  num_iterations = 9999, # un numero muy grande, lo limita early_stopping_rounds
+PARAM$lgb_basicos <- list( 
+  boosting= "gbdt",               #puede ir  dart  , ni pruebe random_forest
+  objective= "binary",
+  metric= "custom",
+  first_metric_only= TRUE,
+  boost_from_average= TRUE,
+  feature_pre_filter= FALSE,
+  force_row_wise= TRUE,           #para que los alumnos no se atemoricen con tantos warning
+  verbosity= -100,
+  max_depth=  -1L,                # -1 significa no limitar,  por ahora lo dejo fijo
+  min_gain_to_split= 0.0,         # min_gain_to_split >= 0.0
+  min_sum_hessian_in_leaf= 0.001, #  min_sum_hessian_in_leaf >= 0.0
+  lambda_l1= 0.0,                 # lambda_l1 >= 0.0
+  lambda_l2= 0.0,                 # lambda_l2 >= 0.0
+  max_bin= 31L,                   #lo debo dejar fijo, no participa de la BO
+  num_iterations= 9999,           #un numero muy grande, lo limita early_stopping_rounds
 
-  bagging_fraction = 1.0, # 0.0 < bagging_fraction <= 1.0
-  pos_bagging_fraction = 1.0, # 0.0 < pos_bagging_fraction <= 1.0
-  neg_bagging_fraction = 1.0, # 0.0 < neg_bagging_fraction <= 1.0
-  is_unbalance = FALSE, #
-  scale_pos_weight = 1.0, # scale_pos_weight > 0.0
+  bagging_fraction= 1.0,          # 0.0 < bagging_fraction <= 1.0
+  pos_bagging_fraction= 1.0,      # 0.0 < pos_bagging_fraction <= 1.0
+  neg_bagging_fraction= 1.0,      # 0.0 < neg_bagging_fraction <= 1.0
+  is_unbalance=  FALSE,           # 
+  scale_pos_weight= 1.0,          # scale_pos_weight > 0.0
 
-  drop_rate = 0.1, # 0.0 < neg_bagging_fraction <= 1.0
-  max_drop = 50, # <=0 means no limit
-  skip_drop = 0.5, # 0.0 <= skip_drop <= 1.0
+  drop_rate=  0.1,                # 0.0 < neg_bagging_fraction <= 1.0
+  max_drop= 50,                   # <=0 means no limit
+  skip_drop= 0.5,                 # 0.0 <= skip_drop <= 1.0
 
-  extra_trees = TRUE, # Magic Sauce
+  extra_trees= TRUE,             # Magic Sauce
 
-  seed = PARAM$lgb_semilla
+  seed=  PARAM$lgb_semilla
 )
 
 
 # Aqui se cargan los hiperparametros que se optimizan
 #  en la Bayesian Optimization
-PARAM$bo_lgb <- makeParamSet(
-  makeNumericParam("learning_rate", lower = 0.02, upper = 0.3),
-  makeNumericParam("feature_fraction", lower = 0.01, upper = 1.0),
-  makeIntegerParam("num_leaves", lower = 8L, upper = 1024L),
-  makeIntegerParam("min_data_in_leaf", lower = 100L, upper = 50000L)
+PARAM$bo_lgb <- makeParamSet( 
+  makeNumericParam("learning_rate",    lower=    0.01, upper=     0.3),
+  makeNumericParam("feature_fraction", lower=    0.01, upper=     1.0),
+  makeIntegerParam("num_leaves",       lower=    4L,   upper=  1024L),
+  makeIntegerParam("min_data_in_leaf", lower=    1L,   upper= 50000L)
 )
 
 
