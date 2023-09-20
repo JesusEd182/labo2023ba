966d0a7ad6505b804134234c5e0528f90eae893a
JesusEd182 <vielma.jesused182@gmail.com>
2023-09-20 12:51:02 +0000

    601_RUN_jesus_004

---
+++ src/workflow-inicial/Experimento_008_jesus_004/601_RUN_correr_workflow_experimento_008_jesus_004.r
@@ -0,0 +1,20 @@
+# Corrida general del workflow
+
+options(error = function() {
+  traceback(20)
+  options(error = NULL)
+  stop("exiting after script error")
+})
+
+
+# corrida de cada paso del workflow
+
+# primeros pasos, relativamente rapidos
+source("~/labo2023ba/src/workflow-inicial/Experimento_008_jesus_004/611_CA_reparar_dataset_experimento_008_jesus_004.r")
+source("~/labo2023ba/src/workflow-inicial/Experimento_008_jesus_004/621_DR_corregir_drifting_experimento_008_jesus_004.r")
+source("~/labo2023ba/src/workflow-inicial/Experimento_008_jesus_004/631_FE_historia_experimento_008_jesus_004.r")
+source("~/labo2023ba/src/workflow-inicial/Experimento_008_jesus_004/641_TS_training_strategy_experimento_008_004.r")
+
+# ultimos pasos, muy lentos
+source("~/labo2023ba/src/workflow-inicial/Experimento_008_jesus_004/651_HT_lightgbm_experimento_008_jesus_004.r")
+source("~/labo2023ba/src/workflow-inicial/Experimento_008_jesus_004/661_ZZ_final_experimento_008_jesus_004.r")
